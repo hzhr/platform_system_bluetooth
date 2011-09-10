cc61f2c832ce089616dcadae186a6e6488f2c070
Master Huang <hzhrong@gmail.com>
2011-09-10 23:49:54 +0800

    Add support for M8 SE.

--- bluedroid/bluetooth.c
+++ bluedroid/bluetooth.c
@@ -137,6 +137,154 @@ out:
     return ret;
 }
 
+static const char* m8se_path="/proc/m8_se";
+static const char* m8fe_path="/proc/m8_fe";
+
+extern int init_module(void *, unsigned long, const char *);
+extern int delete_module(const char *, unsigned int);
+extern void *load_file(const char *fn, unsigned *_sz); /* system/core/libcutils/load_file.c */
+
+static int m8_checkse()
+{
+	static int detected = 0;
+	static int m8_se = 0;
+
+	if (!detected) {
+		if (0 == access(m8se_path, F_OK))
+		{
+			m8_se = 1;
+		}
+		detected = 1;
+	}
+
+	return m8_se;
+}
+
+static int insmod(const char *filename, const char *args)
+{
+    void *module;
+    unsigned int size;
+    int ret;
+
+    module = load_file(filename, &size);
+    if (!module)
+        return -1;
+
+    ret = init_module(module, size, args);
+
+    free(module);
+
+    return ret;
+}
+
+static int rmmod(const char *modname)
+{
+    int ret = -1;
+    int maxtry = 10;
+
+    while (maxtry-- > 0) {
+        ret = delete_module(modname, O_NONBLOCK | O_EXCL);
+        if (ret < 0 && errno == EAGAIN)
+            usleep(500000);
+        else
+            break;
+    }
+
+    if (ret != 0)
+        LOGD("Unable to unload driver module \"%s\": %s\n",
+             modname, strerror(errno));
+    return ret;
+}
+
+#define DRIVER_MODULE_NAME	"btmrvl_sdio"
+#define DRIVER_MODULE_PATH      "/system/lib/modules/btmrvl_sdio.ko"
+#define DRIVER_MODULE_ARG          ""
+#define DRIVER_MODULE_TAG		DRIVER_MODULE_NAME
+#define MODULE_FILE				"/proc/modules"
+#define DRIVER_LOADER_DELAY	1000000
+
+static int check_driver_loaded() {
+    char driver_status[PROPERTY_VALUE_MAX];
+    FILE *proc;
+    char line[sizeof(DRIVER_MODULE_TAG)+10];
+
+    if ((proc = fopen(MODULE_FILE, "r")) == NULL) {
+        LOGW("Could not open %s: %s", MODULE_FILE, strerror(errno));
+        return 0;
+    }
+    while ((fgets(line, sizeof(line), proc)) != NULL) {
+        if ((strncmp(line, DRIVER_MODULE_TAG, strlen(DRIVER_MODULE_TAG)) == 0) ||
+			(strncmp(line, DRIVER_MODULE_TAG, strlen(DRIVER_MODULE_TAG)) == 0)) {
+            fclose(proc);
+            return 1;
+        }
+    }
+    fclose(proc);
+    return 0;
+}
+
+static int bt_unload_driver()
+{
+	int count = 20; /* wait at most 10 seconds for completion */
+	int ret;
+
+	LOGI("bt_unload_driver");
+
+	if (m8_checkse()) {
+		ret = rmmod(DRIVER_MODULE_NAME);
+	} else {
+		//ret = rmmod(DRIVER_MODULE_NAME_FE);
+		ret = -1;
+	}
+
+	if (ret == 0) {
+		while (count-- > 0) {
+		    if (!check_driver_loaded())
+			break;
+		    usleep(500000);
+		}
+		if (count) {
+			return 0;
+		}
+		return -1;
+	} else
+		return -1;
+}
+
+static int bt_load_driver()
+{
+	int count = 100; /* wait at most 20 seconds for completion */
+
+	LOGI("bt_load_driver");
+
+	if (check_driver_loaded()) {
+		return 0;
+	}
+
+	if (m8_checkse()) {
+		if (insmod(DRIVER_MODULE_PATH, DRIVER_MODULE_ARG) < 0)
+			return -1;
+	} else {
+		//if (insmod(DRIVER_MODULE_PATH, DRIVER_MODULE_ARG) < 0)
+		return -1;
+	}
+
+	usleep(DRIVER_LOADER_DELAY);
+	sched_yield();
+
+	while (count-- > 0) {
+		if (check_driver_loaded())
+			break;
+		usleep(500000);
+		if (count) {
+			bt_unload_driver();
+			return -1;
+		}
+	}
+
+	return 0;
+}
+
 static inline int create_hci_sock() {
     int sk = socket(AF_BLUETOOTH, SOCK_RAW, BTPROTO_HCI);
     if (sk < 0) {
@@ -155,9 +303,16 @@ int bt_enable() {
 
     if (set_bluetooth_power(1) < 0) goto out;
 
+    if (bt_load_driver() < 0) {
+        set_bluetooth_power(0);
+        goto out;
+    }
+
     LOGI("Starting hciattach daemon");
     if (property_set("ctl.start", "hciattach") < 0) {
         LOGE("Failed to start hciattach");
+        bt_unload_driver();
+        set_bluetooth_power(0);
         goto out;
     }
 
@@ -206,12 +361,17 @@ int bt_disable() {
     usleep(HCID_STOP_DELAY_USEC);
 
     hci_sock = create_hci_sock();
-    if (hci_sock < 0) goto out;
+    if (hci_sock < 0) goto err_out;
     ioctl(hci_sock, HCIDEVDOWN, HCI_DEV_ID);
 
     LOGI("Stopping hciattach deamon");
     if (property_set("ctl.stop", "hciattach") < 0) {
         LOGE("Error stopping hciattach");
+        goto out;
+    }
+
+err_out:
+    if (bt_unload_driver() < 0) {
         goto out;
     }
 
